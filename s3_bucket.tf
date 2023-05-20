245d6df4ff8f9c482be4b483bc286e49749a334c
Chris Rose <offline@offby1.net>
2023-05-19 19:54:31 -0700

    Fix deprecated parameters in the bucket

--- s3_bucket.tf
+++ s3_bucket.tf
@@ -1,16 +1,18 @@
 resource "aws_s3_bucket" "logs" {
   bucket        = local.bucket
   force_destroy = local.force_destroy
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm     = local.server_side_encryption_configuration_bucket_key_sse_algorithm
-        kms_master_key_id = local.server_side_encryption_configuration_bucket_key_kms_master_key_id
-      }
-      bucket_key_enabled = local.server_side_encryption_configuration_bucket_key_enabled
+  tags          = local.tags
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "logs" {
+  bucket = aws_s3_bucket.logs.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = local.server_side_encryption_configuration_bucket_key_sse_algorithm
+      kms_master_key_id = local.server_side_encryption_configuration_bucket_key_kms_master_key_id
     }
+    bucket_key_enabled = local.server_side_encryption_configuration_bucket_key_enabled
   }
-  tags = local.tags
 }
 
 resource "aws_s3_bucket_ownership_controls" "logs" {
